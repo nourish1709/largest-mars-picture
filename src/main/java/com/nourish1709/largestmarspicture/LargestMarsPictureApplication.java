93a7bda011e2d4541a6c602c6414dbcdd1ed1d8c
Yevhenii Savonenko <yevs@jacobianengineering.com>
2022-10-05 07:59:25 +0300

    Initial commit

---
+++ src/main/java/com/nourish1709/largestmarspicture/LargestMarsPictureApplication.java
@@ -0,0 +1,13 @@
+package com.nourish1709.largestmarspicture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LargestMarsPictureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LargestMarsPictureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nourish1709/largestmarspicture/LargestMarsPictureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nourish1709.largestmarspicture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LargestMarsPictureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
